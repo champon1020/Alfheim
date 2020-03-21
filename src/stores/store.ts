b3095989a24b5e7b9c5ea1a1bfa405d32e453234
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2020-03-21 17:04:32 +0900

    delete sidebar reducer

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,19 +1,8 @@
 import { createStore, AnyAction, Store } from "redux";
-import { ArticleType, DraftType } from "../type";
+import { DraftType } from "../type";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import rootReducer from "../reducers/rootReducer";
-
-export type ArticlesState = {
-  articles: ArticleType[];
-}
-
-export type SidebarState = {
-  recommendArticles: Array<{
-    id: number;
-    title: string;
-  }>;
-}
 
 export type ManageState = {
   article: DraftType;
@@ -29,7 +18,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = createStore(
   persistedReducer,
-  (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+  // (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
 ) as Store<any, AnyAction>;
 
 export type RootState = ReturnType<typeof store.getState>
