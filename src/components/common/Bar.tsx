5316bb987edcac7adbf457a0b40889bfc0032e38
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2020-10-13 05:14:41 +0900

    update bar item icons (#55)

--- src/components/common/Bar.tsx
+++ src/components/common/Bar.tsx
@@ -2,10 +2,10 @@ import * as React from "react";
 import styled from "styled-components";
 import BarItem from "./BarItem";
 import TwitterIcon from "../../assets/images/accounts/twitter.svg";
-import QiitaIcon from "../../assets/images/accounts/qiita.png";
-import WantedlyIcon from "../../assets/images/accounts/wantedly.svg";
 import LinkedinIcon from "../../assets/images/accounts/linkedin.png";
 import GithubIcon from "../../assets/images/accounts/github.png";
+// import QiitaIcon from "../../assets/images/accounts/qiita.png";
+// import WantedlyIcon from "../../assets/images/accounts/wantedly.svg";
 import Accounts from "../../private/accounts.json";
 
 const BarStyled = styled.div`
@@ -44,14 +44,14 @@ const Bar = () => {
           icon={GithubIcon} 
           href={Accounts.github}
           num={3} />
-        <BarItem 
+        {/* <BarItem 
           icon={WantedlyIcon} 
           href={Accounts.wantedly}
           num={4} />
         <BarItem 
           icon={QiitaIcon}
           href={Accounts.qiita}
-          num={5} />
+          num={5} /> */}
       </SnsLinkListStyled>
     </BarStyled>
   );
